716f2c214506a66289627550fb50f44989a3dabe
Suyog Desai <dsuyogonline@gmail.com>
2026-08-22 21:15:40 -0700

    typo errors in bronze sql: resolved.

--- data/BQ/Bronze_external_table_query.sql
+++ data/BQ/Bronze_external_table_query.sql
@@ -7,19 +7,19 @@ OPTIONS (
 CREATE EXTERNAL TABLE IF NOT EXISTS  `project-66ab2fa5-e082-4e6e-9c4.bronze_dataset.encounters_ha` 
 OPTIONS (
   FORMAT = 'json',
-  URIS = ['gs://healthcare-bucket-8826/landing/hospital-a/encounters*.json']
+  URIS = ['gs://healthcare-bucket-8826/landing/hospital-a/encounters/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS  `project-66ab2fa5-e082-4e6e-9c4.bronze_dataset.patients_ha` 
 OPTIONS (
   FORMAT = 'json',
-  URIS = ['gs://healthcare-bucket-8826/landing/hospital-a/patients*.json']
+  URIS = ['gs://healthcare-bucket-8826/landing/hospital-a/patients/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS  `project-66ab2fa5-e082-4e6e-9c4.bronze_dataset.providers_ha` 
 OPTIONS (
   FORMAT = 'json',
-  URIS = ['gs://healthcare-bucket-8826/landing/hospital-a/providers*.json']
+  URIS = ['gs://healthcare-bucket-8826/landing/hospital-a/providers/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS  `project-66ab2fa5-e082-4e6e-9c4.bronze_dataset.transcations_ha` 
